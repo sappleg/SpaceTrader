43e05d757dc5191c18edc1ef0013edd3294192c4
spencerapplegate <spencer.applegate3@gmail.com>
2012-10-08 12:34:30 -0400

    Added some enumeration to Ship.java, not sure if it right....no Eclipse

--- src/Ship.java
+++ src/Ship.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 
 public class Ship {
 	
+    private ShipType currentShip;
+
 	private int flea = 0;
 	private int gnat = 1;
 	private int firefly = 2;
@@ -13,9 +15,13 @@ public class Ship {
 	private int termite = 8;
 	private int wasp = 9;
 		
+    public enum ShipType {
+        FLEA, GNAT, FIREFLY, MOSQUITO, BUMBLEBEE, BEETLE, HORNET, GRASSHOPPER, TERMITE, WASP
+    }
+
 	public Ship()
 	{
-
+        this.currentShip = FLEA;
 	}
 
 }
