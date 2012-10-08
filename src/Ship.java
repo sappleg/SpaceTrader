37485b0128419586700c70c4b3f24636bb829735
spencerapplegate <spencer.applegate3@gmail.com>
2012-10-08 12:41:43 -0400

    Removed hard tabs in ship.java

--- src/Ship.java
+++ src/Ship.java
@@ -4,22 +4,22 @@ public class Ship {
 	
     private ShipType currentShip;
 
-	private int flea = 0;
-	private int gnat = 1;
-	private int firefly = 2;
-	private int mosquito = 3;
-	private int bumblebee = 4;
-	private int beetle = 5;
-	private int hornet = 6;
-	private int grasshopper = 7;
-	private int termite = 8;
-	private int wasp = 9;
-		
+    private int flea = 0;
+    private int gnat = 1;
+    private int firefly = 2;
+    private int mosquito = 3;
+    private int bumblebee = 4;
+    private int beetle = 5;
+    private int hornet = 6;
+    private int grasshopper = 7;
+    private int termite = 8;
+    private int wasp = 9;
+
     public enum ShipType {
         FLEA, GNAT, FIREFLY, MOSQUITO, BUMBLEBEE, BEETLE, HORNET, GRASSHOPPER, TERMITE, WASP
     }
 
-	public Ship(ShipType ship) {
+    public Ship(ShipType ship) {
         this.currentShip = ship;
-	}
+    }
 }
