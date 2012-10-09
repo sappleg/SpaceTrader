612246b1adf7d5f89b68f920cf4178c0c4ce7bf6
Shawn Chaudhry <shawnichaudhry@gmail.com>
2012-10-09 00:06:43 -0400

    Commented. Sorry about that!

--- src/model/Player.java
+++ src/model/Player.java
@@ -23,36 +23,67 @@ public class Player {
 		engineer = en;
 	}
 	
+	/**
+	 * Sets the ship type.
+	 * @param sh the ship type.
+	 */
 	public void setShip(ShipType sh)
 	{
 		ship = sh;
 	}
 	
+	/**
+	 * Gets the ship type.
+	 * @return the ship type.
+	 */
 	public ShipType getShip()
 	{
 		return ship;
 	}
 	
+	/**
+	 * Gets the amount of money the player has currently.
+	 * @return an int that is the amount of currency.
+	 */
 	public static int getCurrency()
 	{
 		return currency;
 	}
 	
+	/**
+	 * Set the currency to be a particular value.
+	 * @param curr an int that is the amount of money.
+	 */
 	public void setCurrency(int curr)
 	{
 		currency = curr;
 	}
 	
+	/**
+	 * Get the player's name.
+	 * @return String that contains the player's name.
+	 */
 	public static String getName()
 	{
 		return name;
 	}
 	
+	/**
+	 * Set the player's name.
+	 * @param nm - String - set the player's name to this.
+	 */
 	public void setName(String nm)
 	{
 		name = nm;
 	}
 	
+	/**
+	 * Set the skill points.
+	 * @param pi - pilot skill points
+	 * @param fi - fighter skill points
+	 * @param tr - trader skill points
+	 * @param en - engineer skill points.
+	 */
 	public void setSkills(int pi, int fi, int tr, int en)
 	{
 		pilot = pi;
