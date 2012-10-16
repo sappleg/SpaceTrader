cad2c4326ef44356b3b8872acc5f3b2b03af4221
VrajPatel <vraj690@gmail.com>
2012-10-15 23:31:58 -0400

    # Added comments to generateCoordinates method.

--- src/presenter/Driver.java
+++ src/presenter/Driver.java
@@ -7,135 +7,138 @@ import main.Ship;
 import model.Player;
 
 public class Driver {
-    Player playerOne;
-    static int currCurrency = 1000;
-    static ArrayList<Integer> totalSkills;
-    ArrayList<Integer> partySkills;
-    ArrayList<Integer> X;
-    ArrayList<Integer> Y;
-    
-    
-    public Driver()
-    {
-	totalSkills = new ArrayList<Integer>();
-	partySkills = new ArrayList<Integer>();
-    }
-    /**
-     * Creates the universe that the game will be played in.
-     */
-    public void generateUniverse() {
+	Player playerOne;
+	static int currCurrency = 1000;
+	static ArrayList<Integer> totalSkills;
+	ArrayList<Integer> partySkills;
+	ArrayList<Integer> X;
+	ArrayList<Integer> Y;
 
-    }
-
-    /**
-     * Creates list of usable X and Y coordinates for solar systems.
-     * That are at least 10 units away from any other System.
-     */
-    public void generateCoordinates(){
-    	
-    	int amtOfCoordinates = 150;			//Amount of coordinates to generate
-        ArrayList<Integer> X = new ArrayList<Integer>();
-        ArrayList<Integer> Y = new ArrayList<Integer>();
-
-    	
-    	
-    	while(amtOfCoordinates > 0){
-    		boolean goodCoords = false;
-        	int arrayLength = X.size();				//List of arrayLength 
-        	
-    		while(goodCoords!=true){
-    			int trueCounter = 0;
-    			int testX = (int) (Math.random()*201);
-    			int testY = (int) (Math.random()*201);
-    		
-    			for(int i=0; i<arrayLength; i++){
-    				int tempX = X.get(i);
-    				int tempY = Y.get(i);
-    			
-    				int distance = (int)((Math.pow(tempX-testX, 2)) +(Math.pow(tempY-testY, 2)));
-    				distance = (int) (Math.sqrt(distance));
-    				
-    				if (distance >= 10){
-    					trueCounter++;
-    				}
-    			}
-    			
-    			if (trueCounter==arrayLength){
-    				X.add(testX);
-    				Y.add(testY);
-    				goodCoords=true;
-    			}
-    		}
-    		amtOfCoordinates++;
-    	}
-    	
-    	
-    	
-    	
-    }
-    
-    
-    /**
-     * Creates the player
-     * 
-     * @param name
-     *            - the name of the player
-     * @param traderUsed
-     *            - points assigned for trader
-     * @param engineerUsed
-     *            - points assigned for engineer
-     * @param pilotUsed
-     *            - points assigned for pilot
-     * @param fighterUsed
-     *            - points assigned for fighter
-     */
-    public void createPlayer(String name, int traderUsed, int engineerUsed,
-	    int pilotUsed, int fighterUsed) {
-	playerOne = new Player(currCurrency, Ship.ShipType.GNAT, name,
-		pilotUsed, fighterUsed, traderUsed, engineerUsed);
-	totalSkills.add(traderUsed);
-	totalSkills.add(engineerUsed);
-	totalSkills.add(pilotUsed);
-	totalSkills.add(fighterUsed);
-    }
-
-    /**
-     * Returns the current player's money.
-     * 
-     * @return an int that contains the player's money.
-     */
-    public static int getCurrency() {
-	return currCurrency;
-    }
-
-    /**
-     * Gets the entire party's skills.
-     * 
-     * @return an ArrayList<Integer> that contains all the skills.
-     */
-    public static ArrayList<Integer> getSkills() {
-	return totalSkills;
-    }
-
-    /**
-     * This method is to boost the player's statistics in case he/she purchases
-     * a mercenary.
-     * 
-     * @param tr
-     *            number of trader points.
-     * @param en
-     *            number of engineer points.
-     * @param pi
-     *            number of pilot points.
-     * @param fi
-     *            number of fighter points.
-     */
-    public void boostSkillPoints(int tr, int en, int pi, int fi) {
-	int[] boostPoints = { tr, en, pi, fi };
-	ArrayList<Integer> newTotalSkills = new ArrayList<Integer>();
-	for (int i = 0; i < boostPoints.length; i++) {
-	    newTotalSkills.add(totalSkills.get(i) + boostPoints[i]);
+	public Driver() {
+		totalSkills = new ArrayList<Integer>();
+		partySkills = new ArrayList<Integer>();
 	}
-	totalSkills = newTotalSkills;
-    }
+
+	/**
+	 * Creates the universe that the game will be played in.
+	 */
+	public void generateUniverse() {
+
+	}
+
+	/**
+	 * Creates list of usable X and Y coordinates for solar systems. That are at
+	 * least 10 units away from any other coordinate.
+	 */
+	public void generateCoordinates() {
+
+		int amtOfCoordinates = 150; //Amount of coordinates to generate
+		ArrayList<Integer> X = new ArrayList<Integer>(); //List to hold X coordinates
+		ArrayList<Integer> Y = new ArrayList<Integer>(); //Lost to hold Y coordinates
+
+		
+		while (amtOfCoordinates > 0) { //Will loop until proper amt of coordinates have been generated.
+			boolean goodCoords = false;
+			int arrayLength = X.size(); //Updates the length of the ArrayList
+
+			while (goodCoords != true) { //Will loop until good coordinates have been found.
+				int trueCounter = 0;
+				//Randomly picks two integers for X and Y.
+				int testX = (int) (Math.random() * 201);
+				int testY = (int) (Math.random() * 201);
+					//Goes through current array to make sure randomly picked X and Y are 
+					// at a distance of 10 or more then all other coordinates.
+				for (int i = 0; i < arrayLength; i++) {
+					int tempX = X.get(i);
+					int tempY = Y.get(i);
+
+					int distance = (int) ((Math.pow(tempX - testX, 2)) + (Math
+							.pow(tempY - testY, 2)));
+					distance = (int) (Math.sqrt(distance));
+					//The randomly picked numbers are >= to coordinates at index i then add to
+					//the trueCounter. Else break and generate new coordinates.
+					if (distance >= 10) {
+						trueCounter++;
+					}
+					else{
+						break;
+					}
+				}
+				if (trueCounter == arrayLength) {
+					X.add(testX);
+					Y.add(testY);
+					goodCoords = true;
+				}
+			}
+			amtOfCoordinates--;
+		}
+
+		this.X = X; 
+		this.Y = Y;
+
+	}
+
+	/**
+	 * Creates the player
+	 * 
+	 * @param name
+	 *            - the name of the player
+	 * @param traderUsed
+	 *            - points assigned for trader
+	 * @param engineerUsed
+	 *            - points assigned for engineer
+	 * @param pilotUsed
+	 *            - points assigned for pilot
+	 * @param fighterUsed
+	 *            - points assigned for fighter
+	 */
+	public void createPlayer(String name, int traderUsed, int engineerUsed,
+			int pilotUsed, int fighterUsed) {
+		playerOne = new Player(currCurrency, Ship.ShipType.GNAT, name,
+				pilotUsed, fighterUsed, traderUsed, engineerUsed);
+		totalSkills.add(traderUsed);
+		totalSkills.add(engineerUsed);
+		totalSkills.add(pilotUsed);
+		totalSkills.add(fighterUsed);
+	}
+
+	/**
+	 * Returns the current player's money.
+	 * 
+	 * @return an int that contains the player's money.
+	 */
+	public static int getCurrency() {
+		return currCurrency;
+	}
+
+	/**
+	 * Gets the entire party's skills.
+	 * 
+	 * @return an ArrayList<Integer> that contains all the skills.
+	 */
+	public static ArrayList<Integer> getSkills() {
+		return totalSkills;
+	}
+
+	/**
+	 * This method is to boost the player's statistics in case he/she purchases
+	 * a mercenary.
+	 * 
+	 * @param tr
+	 *            number of trader points.
+	 * @param en
+	 *            number of engineer points.
+	 * @param pi
+	 *            number of pilot points.
+	 * @param fi
+	 *            number of fighter points.
+	 */
+	public void boostSkillPoints(int tr, int en, int pi, int fi) {
+		int[] boostPoints = { tr, en, pi, fi };
+		ArrayList<Integer> newTotalSkills = new ArrayList<Integer>();
+		for (int i = 0; i < boostPoints.length; i++) {
+			newTotalSkills.add(totalSkills.get(i) + boostPoints[i]);
+		}
+		totalSkills = newTotalSkills;
+	}
 }
