b96eecca317b04701f75a87f4ac40e434a2a82f5
Shawn Chaudhry <shawnichaudhry@gmail.com>
2012-10-21 22:36:00 -0400

    Added buttons, textboxes, and columns to the tradegoodsview.

--- src/edu/gatech/statusquo/spacetrader/view/TradeGoodsView.java
+++ src/edu/gatech/statusquo/spacetrader/view/TradeGoodsView.java
@@ -2,10 +2,12 @@ package edu.gatech.statusquo.spacetrader.view;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.TableCursor;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
 
 public class TradeGoodsView {
 	Shell shell;
@@ -14,6 +16,9 @@ public class TradeGoodsView {
 	TableColumn tblclmnPrice;
 	TableColumn tblclmnNewColumn;
 	TableCursor tableCursor_1;
+	TableColumn tblclmnPersonalQty;
+	Text text;
+	Text text_1;
 	
 	public TradeGoodsView(Shell s) {
 		this.shell = s;
@@ -31,22 +36,48 @@ public class TradeGoodsView {
 	}
 	
 	public void createView() {
-		Label label = new Label(shell, SWT.NONE);
+	    Label label = new Label(shell, SWT.NONE);
 		label.setAlignment(SWT.CENTER);
 		label.setText("Trade Goods");
 		label.setBounds(485, 0, 77, 15);
 
-		table_1.setBounds(196, 21, 653, 579);
+		table_1 = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		table_1.setBounds(196, 21, 653, 505);
 		table_1.setHeaderVisible(true);
 		table_1.setLinesVisible(true);
 
-		tblclmnItems.setWidth(521);
+		TableColumn tblclmnItems = new TableColumn(table_1, SWT.NONE);
+		tblclmnItems.setWidth(394);
 		tblclmnItems.setText("Item");
 
-		tblclmnPrice.setWidth(64);
+		TableColumn tblclmnPrice = new TableColumn(table_1, SWT.NONE);
+		tblclmnPrice.setWidth(79);
 		tblclmnPrice.setText("Price");
 
-		tblclmnNewColumn.setWidth(63);
-		tblclmnNewColumn.setText("Quantity");
+		tblclmnPersonalQty = new TableColumn(table_1, SWT.NONE);
+		tblclmnPersonalQty.setWidth(97);
+		tblclmnPersonalQty.setText("Personal Qty");
+
+		tblclmnNewColumn = new TableColumn(table_1, SWT.NONE);
+		tblclmnNewColumn.setWidth(79);
+		tblclmnNewColumn.setText("Market Qty");
+
+		tableCursor_1 = new TableCursor(table_1, SWT.NONE);
+		
+		Button btnBuy = new Button(shell, SWT.NONE);
+		btnBuy.setBounds(348, 554, 75, 25);
+		btnBuy.setText("Buy");
+
+		Button btnSell = new Button(shell, SWT.NONE);
+		btnSell.setBounds(630, 554, 75, 25);
+		btnSell.setText("Sell");
+
+		text = new Text(shell, SWT.BORDER);
+		text.setText("Enter Qty");
+		text.setBounds(266, 558, 76, 21);
+
+		text_1 = new Text(shell, SWT.BORDER);
+		text_1.setText("Enter Qty");
+		text_1.setBounds(548, 558, 76, 21);
 	}
 }
