87c92bc3c130e804d0ed513f46025cd3466f913b
Derrek Keyser <Derrek.Keyser@gmail.com>
2012-10-30 17:21:45 -0400

    #added listener for btnTravel #ran the selection against player location and determined fuel and distance required #built in checks to make sure player has selected planet and has enough fuel

--- src/edu/gatech/statusquo/spacetrader/presenter/LocalPlanetPresenter.java
+++ src/edu/gatech/statusquo/spacetrader/presenter/LocalPlanetPresenter.java
@@ -6,13 +6,13 @@ import edu.gatech.statusquo.spacetrader.model.Ship;
 import edu.gatech.statusquo.spacetrader.model.SolarSystem;
 import edu.gatech.statusquo.spacetrader.view.*;
 import edu.gatech.statusquo.spacetrader.driver.Driver;
-
+import org.eclipse.swt.events.*;
 
 public class LocalPlanetPresenter {
 	Shell shell;
 	Driver driver;
 	LocalPlanetView localPlanetView;
-	final int distancePerFuelUnit = 11;
+	final int DISTANCEPERFUELUNIT = 11;
 	/**
 	 * class constructor using listed parameters
 	 * @param s
@@ -24,11 +24,75 @@ public class LocalPlanetPresenter {
 		this.driver = d;
 		this.localPlanetView = lpv;
 		setNewLocals();
+		setListeners();
 	}
+	
+	private void setListeners() {
+
+		localPlanetView.btnTravel.addMouseListener(new MouseAdapter(){
+			public void mouseUp(MouseEvent e) {
+				/*
+				 * get selection from local planets list
+				 * get players x y
+				 * calculate distance difference
+				 * calculate fuel needed
+				 * check that player has necessary fuel
+				 * change player location
+				 * update player vitals
+				 * update market
+				 * update local planet list
+				 * update ss stats
+				 */
+				
+				if (LocalPlanetView.list_2.getSelection().length == 0) {
+					NotificationsView.list_1.add("Please select an solar system from the list");
+					return;
+				}
+				
+				int selectedPlanet = LocalPlanetView.list_2.getSelectionIndex();
+				String sys = LocalPlanetView.list_2.getItem(selectedPlanet);
+				SolarSystem destination = new SolarSystem();
+				
+				for(int i=0; i < Driver.listOfSystems.size(); i++){
+					SolarSystem temp = Driver.listOfSystems.get(i);
+					String tempName = temp.getSystemName();
+					
+					if(tempName.equals(sys)){
+						destination = temp;
+						break;
+					}
+				}
+				
+				int destinationX = destination.getxLocation();
+				int destinationY = destination.getyLocation();
+				int playerX = Driver.player.getPlayerX();
+				int playerY = Driver.player.getPlayerY();
+				
+				int distance = (int) ((Math.pow(playerX - destinationX, 2)) + (Math
+                        .pow(playerY - destinationY, 2)));
+                distance = (int) (Math.sqrt(distance));
+                
+                int fuelNeeded = distance/DISTANCEPERFUELUNIT;
+                Ship currentShip = Driver.player.getShip();
+                int playerFuel = currentShip.getFuelLevel();
+                
+                if(fuelNeeded > playerFuel){
+                	NotificationsView.list_1.add("You do not have enough fuel, buy more!");
+                	return;
+                }
+                
+                else{
+                	
+                }
+				
+			}
+		});
+	}	
 	
 	/**
 	 * Refreshes the local systems depending on player X and Y location.
 	 */
+	
 	public void setNewLocals(){
 	       /*
          * Take max fuel cap
@@ -39,7 +103,7 @@ public class LocalPlanetPresenter {
 	    
 	        Ship currentShip = Driver.player.getShip();
 	        int fuelCap = currentShip.getFuelCapacity();
-	        int maxDistance = fuelCap * distancePerFuelUnit;
+	        int maxDistance = fuelCap * DISTANCEPERFUELUNIT;
 	        int playerX = Driver.player.getPlayerX();
 	        int playerY = Driver.player.getPlayerY();
 	
